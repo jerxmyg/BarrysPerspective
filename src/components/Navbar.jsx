6fefe11209ff356b2ad5e84c0e65d9687251cc4c
Jeremy Galindo <jeremyg@jeremys-mbp.mynetworksettings.com>
2024-07-02 15:36:29 -0400

    Added the logo as the home button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,14 @@ import React, { useState } from "react";
 
 import "./Navbar.css";
 import { Link, NavLink } from "react-router-dom";
-
+import logo from '../assets/bplogo.png'
 export const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <nav>
       <Link to="/" className="title">
-        bb
+        <img src={logo} alt="Logo" style={{ height: '75px' }} />
       </Link>
       <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
         <span></span>
